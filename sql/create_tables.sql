f3ee4f313460d4be2c3188dc2feba037d0c4e1f1
Bhagyashree <bhagya099@gmail.com>
2021-08-29 14:01:10 +1000

    create table database fortesting

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,9 +1,9 @@
-DROP DATABASE IF EXISTS test_post;
 
-CREATE TABLE IF NOT EXISTS user(
+
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     firstname VARCHAR(255) NOT NULL,
     lastname VARCHAR(255) NOT NULL,
     email     VARCHAR(200) NOT NULL,
     password CHAR(60) NOT NULL
-)
+);
