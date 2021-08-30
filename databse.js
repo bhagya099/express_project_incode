bc0a5fd02907334b6d9b7aa3070ae2faea81d24d
Bhagyashree <bhagya099@gmail.com>
2021-08-30 16:39:27 +1000

    created 3c branch for 3rd challange in Project 3A

---
+++ databse.js
@@ -0,0 +1,7 @@
+const pgp = require("pg-promise")();
+
+const databse = "test_post";
+
+const connection = "postgres://postgres:1990@localhost:5432/database";
+
+module.exports = db;
---
+++ sql/create_tables.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email     VARCHAR(200) NOT NULL,
+    password CHAR(60) NOT NULL
+);
